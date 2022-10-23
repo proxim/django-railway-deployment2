3968f1e7a6a8266844fd19e0b00f70c891d583de
proxim <61657192+proxim@users.noreply.github.com>
2022-10-23 15:19:23 -0500

    add static file

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
-from . import views
+from polls.views import HomeView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', HomeView.as_view(), name='home'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
 from deploythis.settings import ENV_VAR
 
-def index(request):
-    return HttpResponse(ENV_VAR)
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'index.html', context={'heading_text': ENV_VAR})
